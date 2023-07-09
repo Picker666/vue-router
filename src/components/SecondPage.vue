0a54c2c187ba58731ea3c6645cd05f634429a126
Christine-Only <tsy17321289030@163.com>
2023-07-09 16:56:14 +0800

    program router

--- src/components/SecondPage.vue
+++ src/components/SecondPage.vue
@@ -4,12 +4,35 @@
     <router-link to="/secondPage/88/profile">second page 88</router-link>
     <router-link to="/secondPage/66/posts">second page 66</router-link>
     <router-view></router-view>
+    <p>
+      <button @click="handleHomeClick">home</button>
+      <button @click="handle200Click">second page 00</button>
+      <button @click="handle2Click">second page</button>
+
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SecondPage',
+  methods: {
+    handleHomeClick() {
+      this.$router.push('/firstPage')
+    },
+    handle200Click() {
+      this.$router.push({
+        name: 'second profile page',
+        params: {id: 6}
+      })
+    },
+    handle2Click() {
+      this.$router.push({
+        path: '/secondPage/:id',
+        query: { id: 6 }
+      })
+    }
+  },
   watch: {
     $route(to, from) {
       console.log('to, from: ', to, from);
